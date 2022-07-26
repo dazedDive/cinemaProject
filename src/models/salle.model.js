afcefbdddf18472a3a0664dbb67dcfe57e398e95
hugofarfaday <ugolescargot0601@gmail.com>
2022-07-26 11:10:48 +0200

    SalleModel

---
+++ src/models/salle.model.js
@@ -0,0 +1,19 @@
+import { BaseModel } from "./baseModel.model.js"
+import { DataManager } from "./helpers/dataManager.helper";
+
+export class Salle extends BaseModel{
+    
+    id = 1;
+    nbPlacesMax = 0;
+    theme = "";
+ 
+    constructor(props){
+        super(props);
+        this.assign(props);
+    }
+
+    getSeances(){
+        const dataManager = new DataManager();
+        return dataManager.getAll("seances").filter(seances => this.id == seances.salle_id);
+    }
+}
